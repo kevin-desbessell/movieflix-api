0f149d0a9c39a4369fd5b7f00701bbc63b6af8f3
Kevin Desbessell <kevindesbessell@gmail.com>
2024-03-20 14:13:03 -0300

    adicionada a correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -24,6 +24,21 @@ app.post("/movies", async (req, res) => {
 
     const { tittle, genre_id, language_id, oscar_count, release_date } = req.body;
     try {
+
+        // verificar no banco se já existe um filme com o nome que está sendo enviado
+
+        // case insensitive - se a busca for feita por john wick ou John Wick ou JOHN WICK, o registro vai ser retornado na consulta 
+
+        //case sensitive- se buscar por john wick e no banco estiver como John Wick não vai ser retornado na consulta
+
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: { title: {equals: tittle, mode: "insensitive"} },
+        });
+
+        if(movieWithSameTitle) {
+            return res.status(409).send({message: "Já existe eum filme cadastrado com esse título"});
+        }
+
         await prisma.movie.create({
             data: {
                 title: tittle,
